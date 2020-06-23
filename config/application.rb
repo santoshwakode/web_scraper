f6b6e80990b5bf53fdbaf823301919ca68984a66
santoshw_cyb <santoshw@cybage.com>
2020-06-23 12:02:11 +0530

    Adding readme file

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,3 @@
-# require 'bundler'
-# Bundler.require(:default)
-# Dir.glob(File.join('./', '*.rb'), &method(:require))
-# Dir.glob(File.join('./', '{models}', '*.rb'), &method(:require))
-# require_relative '../app/models/scraper'
-# require_relative '../app/scraper_scripts/extract_data'
-
-# require 'active_record'
-# require 'watir'
-
-# require_relative 'db_connection'
-# require_relative 'watir_config'
 
 Dir.glob(File.join('./app', '**', '*.rb'), &method(:require))
 
@@ -36,25 +24,8 @@ module WebScraper
  end
 end
 
-# require 'sidekiq/api' # for the case of rails console
-
-# Sidekiq::Queue.new("infinity").clear
-# Sidekiq::RetrySet.new.clear
-# Sidekiq::ScheduledSet.new.clear
-# Sidekiq::DeadSet.new.clear
-
 require_relative "initializers/sidekiq"
 
-
-#Sidekiq::Client.enqueue_to "default", MyWorker, "param_11" => "1", "param_22" => "2"
-
-#bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e ${RACK_ENV:-development}
-
-#bundle exec sidekiq -L log/sidekiq.log -C config/sidekiq.yml -e ${RACK_ENV:-development}
-
-#bundle exec sidekiq -e ${RACK_ENV:-development} -r ./config/application.rb -C ./config/sidekiq.yml
-
-#bundle exec sidekiq -e ${RACK_ENV:-development} -r ./config/application.rb -C ./config/sidekiq.yml
 
 
 
